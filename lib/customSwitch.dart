94a658384192136cd5654317c75b451cd1b71cbb
caronls <lesliecaron@live.fr>
2024-02-19 17:04:02 +0100

    add switch mangement

--- lib/customSwitch.dart
+++ lib/customSwitch.dart
@@ -1,73 +1,107 @@
+import 'package:astroremote/wifi/server_command.dart';
 import 'package:flutter/material.dart';
 
 class CustomSwitchWidget extends StatefulWidget {
-  String name;
-  bool isActive;
-  void Function(bool) callback;
-
-  CustomSwitchWidget(this.name, this.isActive, this.callback); //constructor
 
   @override
   _CustomSwitch createState() =>
-      _CustomSwitch(name, isActive, isActive, callback);
+      _CustomSwitch();
 }
 
 class _CustomSwitch extends State<CustomSwitchWidget> {
-  String name;
-  bool isActive;
-  bool light;
-  final void Function(bool) callback;
-  _CustomSwitch(
-      this.name, this.isActive, this.light, this.callback); //constructor
+  bool isSuiviOn = false;
+  bool isSuiviSTDOn = false;
+  bool isRetourOn = false;
+
+  void _updateSwitch(int switchNumber) {
+    setState(() {
+      switch (switchNumber) {
+        case 1:
+          isSuiviOn = true;
+          isSuiviSTDOn = false;
+          isRetourOn = false;
+          break;
+        case 2:
+          isSuiviOn = false;
+          isSuiviSTDOn = true;
+          isRetourOn = false;
+          break;
+        case 3:
+          isSuiviOn = false;
+          isSuiviSTDOn = false;
+          isRetourOn = true;
+          break;
+      }
+    });
+  }
+
+  MaterialStateProperty<Color?> GetTrackColor(bool isOn)
+  {
+    return MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) 
+    { return isOn ? Colors.redAccent[700] : Colors.grey[800]; },
+    );
+  }
+
+   MaterialStateProperty<Color?> GetOverlayColor(bool isActive)
+   {
+    return MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states)
+     {return isActive ?  Colors.redAccent[700] : Colors.black;});
+   }
 
   @override
   Widget build(BuildContext context) {
-    final MaterialStateProperty<Color?> trackColor =
-        MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) {
-        // Track color when the switch is selected.
-        if (isActive) {
-          return light ? Colors.redAccent[700] : Colors.grey[800];
-        }
-        // Otherwise return null to set default track color
-        // for remaining states such as when the switch is
-        // hovered, focused, or disabled.
-        return const Color.fromARGB(255, 30, 30, 30);
-      },
-    );
-    final MaterialStateProperty<Color?> overlayColor =
-        MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) {
-        // Material color when switch is selected.
-        if (isActive) {
-          return Colors.redAccent[700];
-        }
-        // Otherwise return null to set default material color
-        // for remaining states such as when the switch is
-        // hovered, or focused.
-        return Colors.black;
-      },
-    );
     return Transform.scale(
         scale: 2,
-        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SizedBox(width: 100, child: Text(name)),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+          const SizedBox(width: 100, child: Text("Suivi")),
           Switch(
             // This bool value toggles the switch.
-            value: light,
-            overlayColor: overlayColor,
-            trackColor: trackColor,
+            value: isSuiviOn,
+            overlayColor: GetOverlayColor(isSuiviOn),
+            trackColor: GetTrackColor(isSuiviOn),
             thumbColor: const MaterialStatePropertyAll<Color>(Colors.black),
-            onChanged: !isActive
-                ? null
-                : (bool value) {
-                    callback(value);
+            onChanged: (bool value) {
+                  _updateSwitch(1);
+                    ServerCommand.suivi(value);
                     setState(() {
-                      light = value;
+                      isSuiviOn = value;
+                    });
+                  },
+          )]),
+          Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+          const SizedBox(width: 100, child: Text("Suivi STD")),
+          Switch(
+            // This bool value toggles the switch.
+            value: isSuiviSTDOn,
+            overlayColor: GetOverlayColor(isSuiviSTDOn),
+            trackColor: GetTrackColor(isSuiviSTDOn),
+            thumbColor: const MaterialStatePropertyAll<Color>(Colors.black),
+            onChanged: (bool value) {
+                  _updateSwitch(2);
+                    ServerCommand.suiviStd(value);
+                    setState(() {
+                      isSuiviSTDOn = value;
+                    });
+                  },
+          )]),
+          Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+          const SizedBox(width: 100, child: Text("Retour")),
+          Switch(
+            // This bool value toggles the switch.
+            value: isRetourOn,
+            overlayColor: GetOverlayColor(isRetourOn),
+            trackColor: GetTrackColor(isRetourOn),
+            thumbColor: const MaterialStatePropertyAll<Color>(Colors.black),
+            onChanged:(bool value) {
+                  _updateSwitch(3);
+                    ServerCommand.retour(value);
+                    setState(() {
+                      isRetourOn = value;
                     });
                   },
           ),
           const SizedBox(width: 5)
-        ]));
+        ])]));
   }
 }
