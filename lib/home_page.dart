31aa06d8b21230b6845fd0a5ce28c6e5eb0f7bf5
Nicolas HERR <nicolas.herr9812@gmail.com>
2024-02-20 19:52:36 +0100

    fix keepalive

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
   }
 }
 
-
 class _HomePage extends State<HomePage> {
   ServerCommand serverCommand = ServerCommand();
   bool isConnected = false;
@@ -22,26 +21,40 @@ class _HomePage extends State<HomePage> {
   bool followStdActivated = true;
   bool returnActivated = true;
   // final info = NetworkInfo();
-  Icon getActualStatus() {
+  var icon = const Icon(Icons.close_rounded, color: Colors.red);
+  void getActualStatus() {
     if (isConnected) {
-      return const Icon(Icons.check_circle_rounded, color: Colors.green);
+      icon = const Icon(Icons.check_circle_rounded, color: Colors.green);
     } else {
-      return const Icon(Icons.close_rounded, color: Colors.red);
+      icon = const Icon(Icons.close_rounded, color: Colors.red);
     }
   }
 
+  _HomePage() {
+    timer(iconButton);
+  }
   SizedBox verticalSpace = const SizedBox(height: 100);
   SizedBox horizontalSpace = const SizedBox(width: 5);
 
-
-  void checkHealth(IconButton iconButton) async {
+  Future<void> checkHealth(IconButton iconButton) async {
     try {
-      await ServerCommand.keepAlive().then((value) => setState(() {
-            isConnected = false;
-            followActivated = false;
-            followStdActivated = false;
-            returnActivated = false;
-          }));
+      return ServerCommand.keepAlive()
+          .then((value) => setState(() {
+                isConnected = true;
+                followActivated = false;
+                followStdActivated = false;
+                returnActivated = false;
+                getActualStatus();
+              }))
+          .catchError((err) => {
+                setState(() {
+                  followActivated = false;
+                  followStdActivated = false;
+                  returnActivated = false;
+                  isConnected = false;
+                  getActualStatus();
+                })
+              });
     } on Exception {
       setState(() {
         followActivated = false;
@@ -55,11 +68,34 @@ class _HomePage extends State<HomePage> {
   Text connectedText() {
     return isConnected ? const Text("Connected") : const Text("Disconnected");
   }
-  
+
+  Future<void> timer(IconButton iconButton) async {
+    print(DateTime.now());
+    try {
+      await checkHealth(iconButton);
+    } on Exception {
+      setState(() {
+        followActivated = false;
+        followStdActivated = false;
+        returnActivated = false;
+        isConnected = false;
+        getActualStatus();
+      });
+    }
+    await Future.delayed(const Duration(seconds: 5), () async {
+      timer(iconButton);
+    });
+    return;
+  }
+
+  IconButton iconButton = const IconButton(
+    icon: Icon(Icons.close_rounded, color: Colors.red),
+    onPressed: null,
+  );
   @override
   Widget build(BuildContext context) {
-    IconButton iconButton = IconButton(
-        icon: getActualStatus(),
+    iconButton = IconButton(
+        icon: icon,
         onPressed: () {
           showDialog(
             context: context,
@@ -72,8 +108,6 @@ class _HomePage extends State<HomePage> {
             },
           );
         });
-    Timer.periodic(
-        const Duration(seconds: 5), (Timer t) => checkHealth(iconButton));
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
--- lib/wifi/server_command.dart
+++ lib/wifi/server_command.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'dart:io' as io;
 
 class ServerCommand {
   static const serverUrl = 'http://192.168.1.11:3000/astroremote/';
@@ -17,6 +18,8 @@ class ServerCommand {
 
   static const speedUrl = '${serverUrl}set_speed';
 
+  static const httpClient = io.HttpClient;
+
   static getHeaders() {
     return <String, String>{'Content-Type': 'application/json; charset=UTF-8'};
   }
@@ -24,7 +27,13 @@ class ServerCommand {
   // Keep alive : should response pong
   // 5 second de timeout
   static Future<http.Response> keepAlive() {
-    return http.get(Uri.parse(keepAliveUrl), headers: getHeaders());
+    try {
+      return http
+          .get(Uri.parse(keepAliveUrl), headers: getHeaders())
+          .onError((onError, err) => throw Exception());
+    } on Exception {
+      return Future.error(Exception('Not connected'));
+    }
   }
 
   static Future<http.Response> connect() {
@@ -34,30 +43,30 @@ class ServerCommand {
   static Future<http.Response> retour(bool on) {
     String url = on ? retourOnUrl : retourOffUrl;
     print(on ? "retour on" : "retour off");
-    if(on) {
+    if (on) {
       suiviStd(false);
       suivi(false);
-      }
+    }
     return http.get(Uri.parse(url), headers: getHeaders());
   }
 
   static Future<http.Response> suiviStd(bool on) {
     String url = on ? suiviStdOnUrl : suiviStdOffUrl;
     print(on ? "std on" : "std off");
-    if(on) {
+    if (on) {
       suivi(false);
       retour(false);
-      }
+    }
     return http.get(Uri.parse(url), headers: getHeaders());
   }
 
   static Future<http.Response> suivi(bool on) {
     String url = on ? suiviOnUrl : suiviOffUrl;
     print(on ? "suivi on" : "suivi off");
-    if(on) {
+    if (on) {
       suiviStd(false);
       retour(false);
-      }
+    }
     return http.get(Uri.parse(url), headers: getHeaders());
   }
 }
